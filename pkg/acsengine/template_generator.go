bbc9edaf19bbf651b74aa9fc820b86184581e3db
Patrick Lang <plang@microsoft.com>
2018-10-03 19:03:04 -0700

    No more escapes since this is going into zip+base64

--- pkg/acsengine/template_generator.go
+++ pkg/acsengine/template_generator.go
@@ -661,7 +661,7 @@ func (t *TemplateGenerator) getTemplateFuncMap(cs *api.ContainerService) templat
 			return fmt.Sprintf("\"customData\": \"%s\"", str)
 		},
 		"GetKubernetesWindowsAgentCustomData": func(profile *api.AgentPoolProfile) string {
-			str, e := t.getSingleLineForTemplate(kubernetesWindowsAgentCustomDataPS1, cs, profile)
+			str, e := t.getSingleLine(kubernetesWindowsAgentCustomDataPS1, cs, profile)
 			if e != nil {
 				panic(e)
 			}
