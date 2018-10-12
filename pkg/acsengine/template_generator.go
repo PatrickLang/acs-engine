93edde75e5838f307bc2688628f8a088b2fe8144
Patrick Lang <plang@microsoft.com>
2018-10-12 13:34:18 -0700

    Adding GetServicePrincipalClientSecretBase64 and using it for PowerShell

--- pkg/acsengine/template_generator.go
+++ pkg/acsengine/template_generator.go
@@ -933,6 +933,8 @@ func (t *TemplateGenerator) getTemplateFuncMap(cs *api.ContainerService) templat
 		"GetServicePrincipalClientSecret": func() string {
 			return cs.Properties.ServicePrincipalProfile.Secret
 		},
-		"windowsEscape": helpers.WindowsEscape,
+		"GetServicePrincipalClientSecretBase64": func() string {
+			return base64.StdEncoding.EncodeToString(cs.Properties.ServicePrincipalProfile.Secret)
+		},
 	}
 }
